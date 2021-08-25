d53239e986a2145535f85b8957d7c0b9cfccdbb5
Trevor <tlist403@gmail.com>
2021-08-24 21:12:47 -0300

    Positioning and clear button

--- script.js
+++ script.js
@@ -16,13 +16,33 @@ function makeGrid(rows, cols) {
 
 makeGrid(16, 16);
 
-function draw() {
+function clearGrid() {
+    const cells = document.querySelectorAll('.grid-item');
+
+    cells.forEach((item) => {
+        
+           item.style.backgroundColor = 'rgb(238, 237, 237)' 
+        
+    });
+    
+}
+
+function clearButton() {
+    const reset = document.querySelector('#clear-button');
+    reset.addEventListener('click', () => {
+        clearGrid();
+    });
+}
+
+function drawNormal() {
     const change = document.querySelectorAll('.grid-item');
     change.forEach((item) => {
         item.addEventListener('mouseover', () => {
           item.style.backgroundColor = '#333';   
-        })
+        });
     }); 
 }
 
-draw();
+
+clearButton();
+drawNormal();
